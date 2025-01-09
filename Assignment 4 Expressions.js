c1b14a50810129e41e0fcf48850d897e0806845b
samebg <samebengho@gmail.com>
2025-01-09 11:02:02 -0800

    Finished

---
+++ Assignment 4 Expressions.js
@@ -0,0 +1,9 @@
+let myFirstName = 'Samuel ';
+let myLastName = 'Ebengho ';
+let otherLastName = 'Lompoko ';
+let myFullName = myFirstName + myLastName + otherLastName;
+let birthYear = 2003;
+let currentYear = 2025;
+let upcomingAge = "this year will be, ";
+
+console.log(myFullName + upcomingAge + (currentYear - birthYear));
